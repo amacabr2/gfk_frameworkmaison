079f59847d012c13910750e3738d7ee5123a6d06
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-09-15 10:15:36 +0200

    Creating a rederer for templates in php

---
+++ src/Framework/Renderer.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amacabr2
+ * Date: 15/09/17
+ * Time: 09:35
+ */
+
+namespace Framework;
+
+
+class Renderer {
+
+    const DEFAULT_NAMESPACE = '__MAIN';
+
+    private $paths = [];
+
+    /**
+     * @param string $namespace
+     * @param null|string $path
+     */
+    public function addPath(string $namespace, ?string $path = null): void {
+        if (is_null($path)) {
+            $this->paths[self::DEFAULT_NAMESPACE] = $namespace;
+        } else {
+            $this->paths[$namespace] = $path;
+        }
+    }
+
+    /**
+     * @param string $view
+     * @return string
+     */
+    public function render(string $view): string {
+        $path = $this->hasNamespace($view) ? $this->replaceNamespace($view) . '.php' : $this->paths[self::DEFAULT_NAMESPACE] . DIRECTORY_SEPARATOR . $view . '.php';
+        ob_start();
+        require($path);
+        return ob_get_clean();
+    }
+
+    /**
+     * @param string $view
+     * @return bool
+     */
+    private function hasNamespace(string $view): bool  {
+        return $view[0] === '@';
+    }
+
+    /**
+     * @param string $view
+     * @return string
+     */
+    private function getNamespace(string $view): string {
+        return substr($view, 1, strpos($view, '/') - 1) ;
+    }
+
+    /**
+     * @param string $view
+     * @return string
+     */
+    private function replaceNamespace(string $view): string  {
+        $namespace = $this->getNamespace($view);
+        return str_replace('@' . $namespace, $this->paths[$namespace], $view);
+    }
+
+}
---
+++ tests/Framework/RendererTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amacabr2
+ * Date: 15/09/17
+ * Time: 09:36
+ */
+
+namespace Tests\Framework;
+
+
+
+use Framework\Renderer;
+use PHPUnit\Framework\TestCase;
+
+class RendererTest extends TestCase {
+
+    /**
+     * @var Renderer
+     */
+    private $renderer;
+
+    public function setUp() {
+        $this->renderer = new Renderer();
+    }
+
+    public function testRenderTheRightPath() {
+        $this->renderer->addPath('blog', __DIR__ .'/views');
+        $content = $this->renderer->render('@blog/demo');
+        $this->assertEquals('Salut', $content);
+    }
+
+    public function testRenderDefaultPath() {
+        $this->renderer->addPath(__DIR__ .'/views');
+        $content = $this->renderer->render('demo');
+        $this->assertEquals('Salut', $content);
+    }
+
+}
